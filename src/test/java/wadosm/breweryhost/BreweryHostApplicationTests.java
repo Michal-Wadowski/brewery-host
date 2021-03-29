321b7e89a1fd2adb56eace298051d4b4d902d4a5
Michał Wadowski <wadosm@gmail.com>
2021-03-29 21:06:49 +0200

    Initial commit (add Spring Boot)

---
+++ src/test/java/wadosm/breweryhost/BreweryHostApplicationTests.java
@@ -0,0 +1,13 @@
+package wadosm.breweryhost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryHostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
